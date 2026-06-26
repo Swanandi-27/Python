4d15326fc25160ebc5a737bef6dd364245e7e229
swanandi-27 <kachiswanandi@gmail.com>
2026-06-26 10:52:19 +0530

    updated math module

--- module/mathmodule.py
+++ module/mathmodule.py
@@ -3,7 +3,7 @@ import math
 print(math.sqrt(25))
 print(math.factorial(5))
 print(math.ceil(45.29))
-print(math.floor(45.01))
+print(math.floor(45.01))#lowest nearer value
 print(math.pow(2,3))#2**3
 print(math.pi)
 
