ab8541e6f695acb0fad1529dd00c5d77fbc2bd38
swanandi-27 <kachiswanandi@gmail.com>
2026-08-15 19:31:41 +0530

    multiple inheritance

---
+++ oops/inheritance/Is-A/multiple/child.py
@@ -0,0 +1,16 @@
+from parent1 import p1
+from parent2 import p2
+class child(p1,p2):
+    def __init__(self,c_name,p1name,p2name):
+        self.c_name=c_name
+        #constructor chaining
+        p1.__init__(self,p1name)
+        p2.__init__(self,p2name)
+
+
+
+
+obj=child("swanandi","suvarna","anup")
+print(obj.c_name)
+print(obj.p1name)
+print(obj.p2name)
